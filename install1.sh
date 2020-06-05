a624b15031db6e913491ea7bad6fd1ccf44cb1f1
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-05 14:50:23 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -3,7 +3,7 @@
 loadkeys hu
 timedatectl set-ntp true
 fdisk /dev/sda
-#mkfs.fat -F32 /dev/sda1
+mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
@@ -11,9 +11,12 @@ mount /dev/sda1 /mnt/boot
 pacstrap /mnt base linux linux-firmware neovim intel-ucode
 mkdir /mnt/home/Data
 mount /dev/sda3 /mnt/home/Data
-chmod 744 /mnt/home/Data
-genfstab -p /mnt >> /mnt/etc/fstab
+chmod 777 /mnt/home/Data
+genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
+bootctl --path=/boot install
+nvim /boot/loader/loader.conf
+nvim /boot/loader/entries/arch.conf
 echo archlinux > /etc/hostname
 echo KEYMAP=hu > /etc/vconsole.conf
 echo LANG=en_US.UTF-8 > /etc/locale.conf
@@ -28,4 +31,3 @@ passwd root
 pacman -S networkmanager
 systemctl enable NetworkManager.service
 exit
-
