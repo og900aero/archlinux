e4a2c69a477cc4c02ca77655fdee82a27971c062
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-05 13:08:47 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -3,12 +3,13 @@
 loadkeys hu
 timdatectl set-ntp true
 fdisk /dev/sda
-#mkfs.fat -F32 /dev/sda1
+mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 #mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 pacstrap /mnt base linux linux-firmware nvim
+mkdir /mnt/home
 mkdir /mnt/home/Data
 mount /dev/sda3 mnt/home/Data
 chmod 777 mnt/home/Data
@@ -21,7 +22,7 @@ export LANG=en_US.UTF-8
 locale-gen
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/Europe/Budapest /etc/localtime
-hwclock –systohc –utc
+hwclock --systohc --utc
 mkinitcpio -p linux
 passwd root
 useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
