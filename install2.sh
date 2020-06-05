bc616000fb58ab9dd5054d6cce7d34105769a2ba
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-05 14:01:18 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-pacman -S xorg-server xorg-apps xorg-xinit
+echo "Shyciii user létrehozása"
+useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
+echo "Shyciii jelszava:"
+passwd shyciii
+pacman -S xorg-server networkmanager
+systemctl enable NetworkManager.service
+chown shyciii:users /mnt/home/Data
 fallocate -l 4096M /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
