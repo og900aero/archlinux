0a5f1cdf6ae9d4e64f40130f180e51a76e4b9415
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-05 14:36:33 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,8 +4,7 @@ echo "Shyciii user létrehozása"
 useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
 echo "Shyciii jelszava:"
 passwd shyciii
-pacman -S xorg-server networkmanager
-systemctl enable NetworkManager.service
+pacman -S xorg-server
 chown shyciii:users /mnt/home/Data
 fallocate -l 4096M /swapfile
 chmod 600 /swapfile
