27d47c1c7b22a248a74cb3ba10c6c1e27e8ef4ff
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-06 19:06:22 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,9 +1,51 @@
 #!/bin/bash
 
-#echo "Shyciii user létrehozása"
-#useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
-#echo "Shyciii jelszava:"
-#passwd shyciii
+set -e
+numberofcores=$(grep -c ^processor /proc/cpuinfo)
+case $numberofcores in
+    16)
+        echo "You have " $numberofcores" cores."
+        echo "Changing the makeflags for "$numberofcores" cores."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j17"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" cores."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 16 -z -)/g' /etc/makepkg.conf
+        ;;
+    8)
+        echo "You have " $numberofcores" cores."
+        echo "Changing the makeflags for "$numberofcores" cores."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j9"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" cores."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/g' /etc/makepkg.conf
+        ;;
+    6)
+        echo "You have " $numberofcores" cores."
+        echo "Changing the makeflags for "$numberofcores" cores."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j7"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" cores."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 6 -z -)/g' /etc/makepkg.conf
+        ;;       
+    4)
+        echo "You have " $numberofcores" cores."
+        echo "Changing the makeflags for "$numberofcores" cores."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j5"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" cores."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 4 -z -)/g' /etc/makepkg.conf
+        ;;
+    2)
+        echo "You have " $numberofcores" cores."
+        echo "Changing the makeflags for "$numberofcores" cores."
+        sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j3"/g' /etc/makepkg.conf
+        echo "Changing the compression settings for "$numberofcores" cores."
+        sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 2 -z -)/g' /etc/makepkg.conf
+        ;;
+    *)
+        echo "We do not know how many cores you have."
+        echo "Do it manually."
+        ;;
+esac
+echo "################################################################"
+echo "###  All cores will be used during building and compression ####"
+echo "################################################################"
 sudo pacman -S --noconfirm xorg-server xorg-xinit xterm
 chown shyciii:users /mnt/home/Data
 fallocate -l 4096M /swapfile
@@ -38,5 +80,6 @@ sudo pacman -Syyu
 trizen -S --noconfirm fuse-zip jmtpfs polybar split2flac-git subversion sacd-extract inxi downgrade
 cd /home/Data/Linux/Compile/st-0.8.2
 make clean install
-
-
+sudo mkdir /etc/pacman.d/hooks
+sudo cp /home/Data/Linux/Backup/etc/pacman.d/hooks/clean_package_cache.hook /etc/pacman.d/hooks/clean_package_cache.hook
+sudo cp /home/Data/Linux/Backup/usr/share/themes/ /usr/share/themes/
