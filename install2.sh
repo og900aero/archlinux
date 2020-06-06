871ca23281d957375d6a2149debe352a654628f9
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-06 16:55:06 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,14 +4,13 @@
 #useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
 #echo "Shyciii jelszava:"
 #passwd shyciii
-pacman -S --noconfirm xorg-server xterm
+pacman -S --noconfirm xorg-server xorg-xinit xterm
 chown shyciii:users /mnt/home/Data
 fallocate -l 4096M /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 sudo pacman -S --noconfirm xclip unrar curlftpfs fzf mediainfo bspwm sxhkd exa i3lock neovim xautolock awesome-terminal-fonts pulseaudio gpicview-gtk3 libreoffice-fresh transmission-gtk bind-tools wget traceroute gnome-calculator bash-completion intel-media-driver vifm ttf-roboto ttf-ubuntu-font-family pacman-contrib blueberry pcmanfm neofetch mpv chromium grsync htop git gimp ntfs-3g gnome-disk-utility android-tools gvfs udiskie rofi caprine
-EDITOR=nvim visudo
 systemctl --user enable pulseaudio
 sudo pacman -S --noconfirm pavucontrol
 systemctl enable bluetooth.service
@@ -37,7 +36,7 @@ cd ..
 rm -rf /home/shyciii/trizen
 sudo pacman -Syyu
 trizen -S --noconfirm fuse-zip, jmtpfs, ttf-ms-fonts polybar split2flac-git sacd-extract inxi downgrade
-cd /home/Data/linux/Compile/st-0.8.2
+cd /home/Data/Linux/Compile/st-0.8.2
 make clean install
 
 
