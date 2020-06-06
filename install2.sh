49480ea029a28bb5d1615b31605e8b80da128bf0
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-06 19:28:22 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -57,7 +57,7 @@ systemctl --user enable pulseaudio
 sudo pacman -S --noconfirm pavucontrol
 systemctl enable bluetooth.service
 systemctl start bluetooth.service
-rfkill unblock bluetooth
+sudo rfkill unblock bluetooth
 sudo pacman -S --noconfirm pulseaudio-bluetooth
 sudo killall pulseaudio
 pulseaudio --start
