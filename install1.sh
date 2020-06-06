d260560239ce678996cf49cc502fb6a4f1db1a72
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-06 16:29:55 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -11,21 +11,21 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
-pacstrap /mnt base linux linux-firmware nano intel-ucode
+pacstrap /mnt base base-devel linux linux-firmware neovim intel-ucode
 mkdir /mnt/home/Data
 mount /dev/sda3 /mnt/home/Data
 chmod 777 /mnt/home/Data
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 bootctl --path=/boot install
-nano /boot/loader/loader.conf
+nvim /boot/loader/loader.conf
 echo -e "Title Arch Linux\nlinux /vmlinuz-linux\ninitrd /intel-ucode.img\ninitrd /initramfs-linux.img\noptions root=PARTUUID=$(blkid | grep sda2 | sed 's/\(.*\)PARTUUID="\(.*\)"$/\2/') rw quit i915.enable_guc=2 loglevel=3" > /boot/loader/entries/arch.conf
 echo -e "127.0.0.1  localhost\n::1  localhost\n127.0.0.1  archlinux" > /etc/hosts
 echo archlinux > /etc/hostname
 echo KEYMAP=hu > /etc/vconsole.conf
 echo LANG=hu_HU.UTF-8 > /etc/locale.conf
 export LANG=hu_HU.UTF-8
-nano /etc/locale.gen
+nvim /etc/locale.gen
 locale-gen
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/Europe/Budapest /etc/localtime
