af72b05787dbcbfb804ece9697e72dd0d9979d81
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-06 18:55:11 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -17,26 +17,3 @@ mount /dev/sda3 /mnt/home/Data
 chmod 777 /mnt/home/Data
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-bootctl --path=/boot install
-nvim /boot/loader/loader.conf
-echo -e "Title Arch Linux\nlinux /vmlinuz-linux\ninitrd /intel-ucode.img\ninitrd /initramfs-linux.img\noptions root=PARTUUID=$(blkid | grep sda2 | sed 's/\(.*\)PARTUUID="\(.*\)"$/\2/') rw quit i915.enable_guc=2 loglevel=3" > /boot/loader/entries/arch.conf
-echo -e "127.0.0.1  localhost\n::1  localhost\n127.0.0.1  archlinux" > /etc/hosts
-echo archlinux > /etc/hostname
-echo KEYMAP=hu > /etc/vconsole.conf
-echo LANG=us_EN.UTF-8 > /etc/locale.conf
-export LANG=us_EN.UTF-8
-nvim /etc/locale.gen
-locale-gen
-rm /etc/localtime
-ln -s /usr/share/zoneinfo/Europe/Budapest /etc/localtime
-hwclock --systohc --utc
-mkinitcpio -p linux
-echo "Root jelszava:"
-passwd root
-pacman -S networkmanager
-systemctl enable NetworkManager.service
-echo "Shyciii user létrehozása"
-useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
-echo "Shyciii jelszava:"
-passwd shyciii
-EDITOR=nvim visudo
