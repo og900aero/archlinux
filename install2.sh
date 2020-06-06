9b694895d3657ab815a6678a5226f5b52cfae14c
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-06 15:58:09 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-echo "Shyciii user létrehozása"
-useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
-echo "Shyciii jelszava:"
-passwd shyciii
-pacman -S xorg-server
+#echo "Shyciii user létrehozása"
+#useradd -m -g users -G audio,video,network,wheel,storage -s /bin/bash shyciii
+#echo "Shyciii jelszava:"
+#passwd shyciii
+pacman -S --noconfirm xorg-server
 chown shyciii:users /mnt/home/Data
 fallocate -l 4096M /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
-EDITOR=nvim visudo
 echo "localectl set keymap hu hu"
-localectl set-keymap hu hu
+#localectl set-keymap hu hu
 sudo pacman -S --noconfirm xclip unrar curlftpfs fzf mediainfo bspwm sxhkd exa i3lock neovim xautolock awesome-terminal-fonts pulseaudio gpicview-gtk3 libreoffice-fresh transmission-gtk bind-tools wget traceroute gnome-calculator bash-completion intel-media-driver vifm ttf-roboto ttf-ubuntu-font-family pacman-contrib blueberry pcmanfm neofetch mpv chromium grsync htop git gimp ntfs-3g gnome-disk-utility android-tools gvfs udiskie rofi caprine
+EDITOR=nvim visudo
 systemctl --user enable pulseaudio
-sudo pacman -S pavucontrol --noconfirm
+sudo pacman -S --noconfirm pavucontrol
 systemctl enable bluetooth.service
 systemctl start bluetooth.service
 Rfkill unblock bluetooth
-sudo pacman -S pulseaudio-bluetooth
+sudo pacman -S --noconfirm pulseaudio-bluetooth
 sudo killall pulseaudio
-pulseaudo --start
+pulseaudio --start
 sudo systemctl restart bluetooth
 sudo usermod -a -G rfkill shyciii
