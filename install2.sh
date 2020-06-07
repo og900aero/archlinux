3aa5399c582cfed0a271ffe0c1e820b86485f9fc
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-07 10:31:41 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -47,11 +47,11 @@ echo "################################################################"
 echo "###  All cores will be used during building and compression ####"
 echo "################################################################"
 sudo pacman -S --noconfirm xorg-server xorg-xinit xterm
-chown shyciii:users /home/Data
-fallocate -l 4096M /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+sudo chown shyciii:users /home/Data
+sudo fallocate -l 4096M /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 sudo pacman -S --noconfirm xclip unrar curlftpfs fzf mediainfo bspwm sxhkd exa i3lock neovim xautolock awesome-terminal-fonts pulseaudio gpicview-gtk3 libreoffice-fresh transmission-gtk bind-tools wget traceroute gnome-calculator bash-completion intel-media-driver vifm ttf-roboto ttf-ubuntu-font-family pacman-contrib blueberry pcmanfm neofetch mpv chromium grsync htop git gimp ntfs-3g gnome-disk-utility android-tools gvfs udiskie rofi caprine
 systemctl --user enable pulseaudio
 sudo pacman -S --noconfirm pavucontrol
@@ -59,7 +59,6 @@ systemctl enable bluetooth.service
 systemctl start bluetooth.service
 sudo rfkill unblock bluetooth
 sudo pacman -S --noconfirm pulseaudio-bluetooth
-sudo killall pulseaudio
 pulseaudio --start
 sudo systemctl restart bluetooth
 sudo usermod -a -G rfkill shyciii
@@ -79,7 +78,7 @@ rm -rf /home/shyciii/trizen
 sudo pacman -Syyu
 trizen -S --noconfirm fuse-zip jmtpfs polybar split2flac-git subversion sacd-extract inxi downgrade
 cd /home/Data/Linux/Compile/st-0.8.2
-make clean install
+sudo make clean install
 sudo mkdir /etc/pacman.d/hooks
 sudo cp /home/Data/Linux/Backup/etc/pacman.d/hooks/clean_package_cache.hook /etc/pacman.d/hooks/clean_package_cache.hook
 sudo cp -r /home/Data/Linux/Backup/usr/share/themes/* /usr/share/themes/
