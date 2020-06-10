527a4c1ae122854394752f13ef8a6837b6d5e53d
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-10 11:19:04 +0200

    Add files via upload

---
+++ install3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm xclip dunst unrar curlftpfs fzf mediainfo bspwm sxhkd exa i3lock neovim xautolock awesome-terminal-fonts pulseaudio gpicview-gtk3 libreoffice-fresh transmission-gtk bind-tools wget traceroute gnome-calculator bash-completion intel-media-driver vifm ttf-roboto ttf-ubuntu-font-family pacman-contrib blueberry pcmanfm neofetch mpv chromium grsync htop git gimp ntfs-3g gnome-disk-utility android-tools gvfs udiskie rofi caprine
+rk-manager-applet
