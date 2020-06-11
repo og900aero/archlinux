898577a094688a003c9171bb0fd5fd4d15f1cf6e
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-11 17:00:22 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -7,7 +7,8 @@ reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 timedatectl set-ntp true
 fdisk /dev/sda
 mkfs.fat -F32 /dev/sda1
-mkfs.ext4 /dev/sda2
+#mkfs.ext4 /dev/sda2
+mkfs.f2fs /dev/sda2
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
@@ -16,4 +17,7 @@ mkdir /mnt/home/Data
 mount /dev/sda3 /mnt/home/Data
 chmod 777 /mnt/home/Data
 genfstab -U /mnt >> /mnt/etc/fstab
+echo "###############################################"
+echo "Töltsd le az Install2.sh scriptet és futtasd le"
+echo "###############################################"
 arch-chroot /mnt
