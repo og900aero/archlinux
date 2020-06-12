dfa083fe2c8dba990e2b4619e6b06cc7c46f16ab
og900aero <49487376+og900aero@users.noreply.github.com>
2020-06-12 11:07:27 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -19,6 +19,6 @@ genfstab -U /mnt >> /mnt/etc/fstab
 cd ..
 cp -r archlinux/ /mnt/home/
 echo "###############################################"
-echo "cd /home/archlinux és Install2.sh-t futtasd le"
+echo "cd /home/archlinux és install2.sh-t futtasd le"
 echo "###############################################"
 arch-chroot /mnt
