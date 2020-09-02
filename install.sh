77f386702b2843c035985dfd289ac8b16248ad1f
og900aero <49487376+og900aero@users.noreply.github.com>
2020-09-02 08:38:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 timedatectl set-ntp true
 fdisk /dev/sda
 mkfs.fat -F32 /dev/sda1
-mkfs.ext4 -F /dev/sda2
+mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
