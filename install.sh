22b538afbeb1ef24d29af2c1f3237b617a17bde9
og900aero <49487376+og900aero@users.noreply.github.com>
2020-10-26 18:24:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,6 +64,7 @@ arch-chroot /mnt swapon /swapfile
 echo "/swapfile   none  swap  defaults,discard  0	0" >> /mnt/etc/fstab
 # Install Xorg
 arch-chroot /mnt pacman -S --noconfirm xorg-server xorg-xinit xorg-fonts-encodings xorg-mkfontscale xterm mesa xf86-video-intel xf86-video-nouveau
+# arch-chroot /mnt pacman -S --noconfirm xorg xterm mesa xf86-video-intel xf86-video-nouveau
 # Install fonts, home dirs etc
 arch-chroot /mnt pacman -S --noconfirm xdg-user-dirs bind wget traceroute man-db man-pages intel-media-driver pacman-contrib bash-completion android-tools gvfs udiskie awesome-terminal-fonts ttf-hack ttf-ubuntu-font-family ttf-roboto ttf-dejavu git ntfs-3g gnome-keyring reflector polkit-gnome
 # Install Sound
