f23a455484db00b1124dbba1b5b99fe70f90e1f5
og900aero <49487376+og900aero@users.noreply.github.com>
2020-11-16 12:23:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,9 +49,9 @@ options  root=PARTUUID=$(blkid | grep sda2 | sed 's/\(.*\)PARTUUID="\(.*\)"$/\2/
 EOF
 
 # Magyar időzóna beállítása
-#ln -sf /mnt/usr/share/zoneinfo/Europe/Budapest /mnt/etc/localtime
+ln -sf /mnt/usr/share/zoneinfo/Europe/Budapest /mnt/etc/localtime
 
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
+#arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Budapest /etc/localtime
 
 # Hardver óra beállítása
 arch-chroot /mnt hwclock --systohc
