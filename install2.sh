7f9601257a13d298be5d61908688fd830b371048
og900aero <49487376+og900aero@users.noreply.github.com>
2020-11-16 13:47:41 +0100

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -62,10 +62,10 @@ Exec = /usr/bin/paccache -r
 EOF
 
 # Swap használatának beállítása
-sudo echo "vm.swappiness=30" > /etc/sysctl.d/99-sysctl.conf
+sudo bash -c "echo vm.swappiness=30 > /etc/sysctl.d/99-sysctl.conf"
 
 # Android Oneplus udev szabály
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="2a70", ATTR{idProduct}=="9011", MODE="0660", GROUP="uucp", ENV{ID_MTP_DEVICE}="1", SYMLINK+="libmtp"' > /etc/udev/rules.d/51-android.rules
+sudo bash -c 'echo SUBSYSTEM=="usb", ATTR{idVendor}=="2a70", ATTR{idProduct}=="9011", MODE="0660", GROUP="uucp", ENV{ID_MTP_DEVICE}="1", SYMLINK+="libmtp" > /etc/udev/rules.d/51-android.rules"
 
 # Login felirat módosítása
 sudo bash -c "cat > /etc/issue" <<EOF
