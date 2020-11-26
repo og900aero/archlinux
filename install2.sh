a5a2e3a67704e99c722caee923af898a21370ec6
Shyciii <og900aero@gmail.com>
2020-11-26 09:52:03 +0100

    Update

--- install2.sh
+++ install2.sh
@@ -27,7 +27,7 @@ pulseaudio --start
 sudo systemctl restart bluetooth
 
 # Programok telepítése hivatalos repóból
-sudo pacman -Sy --noconfirm xclip atool fuse-zip zip unzip unrar curlftpfs fzf mediainfo ueberzug bspwm sxhkd exa i3lock xautolock dunst feh libreoffice-fresh-hu transmission-gtk gnome-calculator vifm blueberry pcmanfm neofetch mpv chromium grsync htop sshfs rofi caprine
+sudo pacman -Sy --noconfirm xclip atool fuse-zip zip unzip unrar curlftpfs fzf mediainfo ueberzug bspwm sxhkd exa i3lock xautolock dunst feh libreoffice-fresh-hu transmission-gtk gnome-calculator vifm blueberry neofetch mpv chromium grsync htop sshfs rofi caprine
 
 # Yay telepítése
 cd /home/shyciii
@@ -37,7 +37,7 @@ makepkg -si
 rm -rf /home/shyciii/yay
 
 # Programok telepítése AUR-ból
-yay -Syyu --noconfirm --sudoloop archivemount polybar split2flac-git subversion sacd-extract inxi downgrade micro
+yay -Syyu --noconfirm --sudoloop go-mtpfs-git archivemount polybar split2flac-git subversion sacd-extract inxi downgrade micro
 
 # Suckless Terminal telepítése
 cd /home/Data/Linux/Compile/st-0.8.4
@@ -68,7 +68,7 @@ EOF
 sudo bash -c "echo vm.swappiness=30 > /etc/sysctl.d/99-sysctl.conf"
 
 # Android Oneplus udev szabály
-sudo bash -c 'echo SUBSYSTEM=="usb", ATTR{idVendor}=="2a70", ATTR{idProduct}=="9011", MODE="0660", GROUP="uucp", ENV{ID_MTP_DEVICE}="1", SYMLINK+="libmtp" > /etc/udev/rules.d/51-android.rules'
+#sudo bash -c 'echo SUBSYSTEM=="usb", ATTR{idVendor}=="2a70", ATTR{idProduct}=="9011", MODE="0660", GROUP="uucp", ENV{ID_MTP_DEVICE}="1", SYMLINK+="libmtp" > /etc/udev/rules.d/51-android.rules'
 
 # Notebook-hoz doube tap beállítása
 sudo bash -c "cat > /etc/X11/xorg.conf.d/40-libinput.conf" <<EOF
@@ -118,9 +118,9 @@ sudo cp -rv /home/Data/Linux/Backup/usr/share/themes/* /usr/share/themes/
 sudo cp -rv /home/Data/Linux/Backup/etc/issue /etc/issue
 atool -fvX ~/ /home/Data/Linux/Backup/home_backup.tar.gz
 cp -rfv /home/Data/Linux/Backup/home/.local/ /home/shyciii/
+mkdir -p /home/shyciii/mnt/android /home/shyciii/mnt/ftp /home/shyciii/mnt/ssh
 #cp -rfv /home/Data/Linux/Backup/home/.config/* /home/shyciii/.config
 #cp -rfv /home/Data/Linux/Backup/home/.grsync/ /home/shyciii/
-#mkdir /home/shyciii/mtp && mkdir /home/shyciii/mtp/android && mkdir /home/shyciii/mtp/ftp && mkdir /home/shyciii/mtp/ssh
 #cp -rfv /home/Data/Linux/Backup/home/Pictures /home/shyciii/
 #cp -fv /home/Data/Linux/Backup/home/.b* /home/Data/Linux/Backup/home/.gt* /home/Data/Linux/Backup/home/.x* /home/shyciii/
 
