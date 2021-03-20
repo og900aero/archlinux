26a2babc9f86d66368f74fb6e0e8c81b03c5adf2
Shyciii <og900aero@gmail.com>
2021-03-20 10:14:43 +0100

    boot i915

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ title    Arch Linux
 linux    /vmlinuz-linux
 initrd   /intel-ucode.img
 initrd   /initramfs-linux.img
-options  root=PARTUUID=$(blkid | grep sda2 | sed 's/\(.*\)PARTUUID="\(.*\)"$/\2/') rw quit i915.enable_guc=2 loglevel=3
+options  root=PARTUUID=$(blkid | grep sda2 | sed 's/\(.*\)PARTUUID="\(.*\)"$/\2/') rw quit i915.enable_guc=2 i915.enable_fbc=1 i915.fastboot=1 loglevel=3
 EOF
 
 # Magyar időzóna beállítása
