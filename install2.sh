150034a3bbd087b5c3948e7d3e7317387e503148
og900aero <49487376+og900aero@users.noreply.github.com>
2021-05-09 14:16:11 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -129,6 +129,4 @@ sudo rm -rf /home/shyciii/go
 # Home könyvtár tulajdonosának visszaállítása
 sudo chown -R shyciii:users /home/shyciii/
 
-# Céges VPN beállítása
-#sudo nmcli connection import type openvpn file /home/Data/_TMVPN/TelemediaOVPN.ovpn
 cd .. && sudo rm -rf /home/archlinux
