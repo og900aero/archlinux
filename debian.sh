186e7c655cc8967f61b74f98282938d9d408e5c4
og900aero <49487376+og900aero@users.noreply.github.com>
2021-12-26 21:26:04 +0100

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,9 +1,9 @@
 sudo sed -i 's/main/main contrib non-free/' /etc/apt/sources.list
 sudo sed -i 's/bullseye/testing/' /etc/apt/sources.list
 sudo apt update
-sudo apt dist-upgrade
-sudo apt install wget curl xserver-xorg-video-intel xinit xserver-xorg-video-dummy xserver-xorg-input-all xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable fonts-font-asesome pulseaudio pavucontrol micro neofetch bspwm polybar rofi vifm rxvt-unicode git imv trash-cli xclip transmission-gtk
-sudo apt install exa dunst unzip unrar-free zip libreoffice htop mediainfo ffmpeg fzf subversion archivemount xsel xbacklight
+sudo apt dist-upgrade -y
+sudo apt install wget curl xserver-xorg-video-intel xinit xserver-xorg-video-dummy xserver-xorg-input-all xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable fonts-font-asesome pulseaudio pavucontrol micro neofetch bspwm polybar rofi vifm xterm git imv trash-cli xclip transmission-gtk -y
+sudo apt install exa dunst unzip unrar-free zip libreoffice htop mediainfo ffmpeg fzf subversion archivemount xsel xbacklight -y
 bash -c "echo "exec bspwm' > /home/shyciii/.xinitrc"
 mkdir -p /home/shyciii/.config/bspwm
 mkdir -p /home/shyciii/.config/sxhkd
@@ -11,6 +11,6 @@ mkdir -p /home/shyciii/.config/rofi
 mkdir -p /home/shyciii/.config/polybar
 cp /home/shyciii/.bashrc /home/shyciii/.bashrc.bak
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.dev
+sudo apt install ./google-chrome-stable_current_amd64.dev -y
 rm ./google-chrome-stable_current_amd64.dev
-sudo apt autoremove vim nano
+sudo apt autoremove vim nano lemonbar -y
